9b69258e2a2da00394300a59ce09559daa4a25d7
skyas <danil.karagudin@mail.ru>
2021-02-02 17:07:57 +0300

    Создал модель базы данных для пользователя(примерную),переписал вид меню,добавил ссылки на кнопки,теперь можно перемещаться между страницами по кнопкам

--- JPT/models.py
+++ JPT/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Persons(models.Model):
+    first_name=models.CharField(max_length=30)
+    second_name=models.CharField(max_length=30)
+    date_of_appearence=models.DateField(auto_now=False, auto_now_add=False)
+    
+
