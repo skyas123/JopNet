a220e0edb822145f3ef8952d93fd3a598fdc899b
skyas <danil.karagudin@mail.ru>
2021-04-25 04:42:53 +0300

    Вроде все шаблоны и вьюху поправил, все работает  на первый взгляд с новым типом модели пользователей. Остальные косяки потом ловить буду

--- JPT/views.py
+++ JPT/views.py
@@ -19,15 +19,16 @@ from django.core import serializers
 # Create your views here.
 def index(request):
   print(request.user)
-  user=User.objects.get(pk=3)
+  user=request.user
   news=Posts.objects.filter(author=user.pk)
+
 
   if 'st' in request.POST:
       formava=ava(request.POST,request.FILES)
       if formava.is_valid() :
           #добавить удаление предыдущей аватарки
           avatar=formava.save()
-          avatar.persons_set.add(persons)
+          avatar.persons_set.add(user.persons)
   else:
      formava = ava 
   
@@ -40,7 +41,7 @@ def index(request):
    if formpost.is_valid():
 
           post=formpost.save()
-          persons.posts_set.add(post)
+          user.persons.posts_set.add(post)
 
    if formpicture.is_valid():
 
@@ -69,11 +70,11 @@ def index(request):
             content_type='html')
  
 
-  return render(request,'JPT/home.html',{'persons':user,'formpost':formpost,'formava':formava,'formpicture':formpicture,'news':news})
+  return render(request,'JPT/home.html',{'user':user,'formpost':formpost,'formava':formava,'formpicture':formpicture,'news':news})
 
-def news(request,user_id):
-  persons=Persons.objects.get(pk=user_id)
-  listOfFriends=Persons.objects.filter(friends=persons.pk)
+def news(request):
+  user=request.user
+  listOfFriends=Persons.objects.filter(friends=user.pk)
   newsRoll=Posts.objects.filter(author__in=listOfFriends)
 
   if 'likebtn' in request.POST:
@@ -87,8 +88,8 @@ def news(request,user_id):
 
   return render(request, 'JPT/news.html',{"newsRoll":newsRoll})
 
-def friends(request,user_id):
-  persons=Persons.objects.get(pk=user_id)
+def friends(request):
+  user=request.user
   
 
 
@@ -99,52 +100,54 @@ def friends(request,user_id):
        #не работают 100% совпадения по поиску доработать
 
        if not len(req):
-        friendsList=Persons.objects.filter(friends=persons.pk)
+        friendsList=User.objects.filter(persons__friends=user.pk)
         response=render(request, 'JPT/resultSearch.html',{"friendsList":friendsList})
         return HttpResponse(response,content_type="html")
 
        else:
-        friendsList=Persons.objects.filter(Q(first_name__in=req)|Q(second_name__in=req))
+        friendsList=User.objects.filter(Q(first_name__in=req)|Q(last_name__in=req))
         response=render(request, 'JPT/resultSearch.html',{"friendsList":friendsList})
         return HttpResponse(response,content_type="html")
 
   else:
-       friendsList=Persons.objects.filter(friends=persons.pk)
+       friendsList=User.objects.filter(persons__friends=user.pk)
        
 
   return render(request, 'JPT/friends.html',{"friendsList":friendsList})
 
 def dialogs(request):
-    persons=Persons.objects.get(pk=9)
-    dialogsList=Dialogs.objects.filter(listOfMembers=persons.pk)
-    return render(request,'JPT/dialogs.html',{"dialogsList":dialogsList,"persons":persons})
+    user=request.user
+    dialogsList=Dialogs.objects.filter(listOfMembers=user.pk)
+    return render(request,'JPT/dialogs.html',{"dialogsList":dialogsList,"persons":user})
 
-def dialog(request, *args,user_id):
-    dlgpk=args[0]
-    persons=Persons.objects.get(pk=user_id)
-    messageList=Message.objects.filter(atachment=dlgpk)
+def dialog(request, *args):
+  dlgpk=args[0]
+  user=request.user
+  Dialogs.objects.get(pk=dlgpk)
 
+  if  Dialogs.objects.get(Q(pk=dlgpk) and Q(listOfMembers=user.pk)): 
+     messageList=Message.objects.filter(atachment=dlgpk)
 
-    if "text" in request.POST:
-     print(request.user)
-     message=messageForm(request.POST)
-     formpicture=photo(request.POST,request.FILES)
-     images=request.FILES.getlist('photo')
+  if "text" in request.POST:
+      print(request.user)
+      message=messageForm(request.POST)
+      formpicture=photo(request.POST,request.FILES)
+      images=request.FILES.getlist('photo')
 
-     if message.is_valid():
-          msg=message.save(commit=False)
-          msg.atachment=Dialogs.objects.get(pk=dlgpk)
-          msg.author=persons
-          msg.save()
+      if message.is_valid():
+           msg=message.save(commit=False)
+           msg.atachment=Dialogs.objects.get(pk=dlgpk)
+           msg.author=Persons.objects.get(pk=user.id)
+           msg.save()
 
-     if formpicture.is_valid():
+      if formpicture.is_valid():
        for p in images:
         picture=Media.objects.create(photo=p)
         #it doesn't clear shoud i use picture.save() or not
         msg.subphoto.add(picture)
 
-    else:
+  else:
       message=messageForm
       formpicture=photo
 
-    return render(request,'JPT/dialog.html',{"messageList": messageList,"persons":persons,"message":messageForm,"formpicture":formpicture})
+  return render(request,'JPT/dialog.html',{"messageList": messageList,"user":user,"message":messageForm,"formpicture":formpicture})
